4ae3aed83be381ad46dd7e667f9bab92aa9970bb
Abira16 <47822500+Abira16@users.noreply.github.com>
2019-12-19 23:18:30 +0530

    Add files via upload

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentDemoExpenseManager.java
@@ -0,0 +1,38 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentMemoryTransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentMemoryAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+import android.content.Context;
+
+/**
+ *
+ */
+public class PersistentDemoExpenseManager extends ExpenseManager {
+    Context context;
+    public PersistentDemoExpenseManager(Context context) {
+        this.context = context;
+        setup();
+    }
+
+    @Override
+    public void setup() {
+        /*** Begin generating dummy data for In-Memory implementation ***/
+
+        TransactionDAO persistentTransactionDAO = new PersistentMemoryTransactionDAO(context);
+        setTransactionsDAO(persistentTransactionDAO);
+
+        AccountDAO persistentAccountDAO = new PersistentMemoryAccountDAO(context);
+        setAccountsDAO(persistentAccountDAO);
+
+        // dummy data
+        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
+        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+        getAccountsDAO().addAccount(dummyAcct1);
+        getAccountsDAO().addAccount(dummyAcct2);
+
+        /*** End ***/
+    }
+}
